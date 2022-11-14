e1cff613c565f0cb1e677069fa691f6a4c79b298
Chris Pucknell <chris.pucknell@gmail.com>
2022-11-14 15:10:34 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const myButton1 = document.getElementById('button-test1')
+const myButton2 = document.getElementById('button-test2')
+
+myButton1.addEventListener('click', () => clickEvent1())
+myButton2.addEventListener('click', () => clickEvent2())
+
+function clickEvent1() {
+    const element = document.getElementById('output')
+    if (element.textContent === "") element.textContent = "Hello World"
+}
+
+function clickEvent2() {
+    if (document.body.style.backgroundColor != 'red') document.body.style.backgroundColor = 'red'
+}
