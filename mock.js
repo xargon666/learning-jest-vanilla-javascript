3f0fc6cf398080fef8b8ca044bfb82538eca179f
Chris Pucknell <chris.pucknell@gmail.com>
2022-11-17 01:41:15 +0000

    mock testing WIP

---
+++ mock.js
@@ -0,0 +1,15 @@
+const myButton = document.getElementById('button-test2')
+myButton.addEventListener('click', () => onClickEvent("button2",popup))
+let count = 0
+
+function onClickEvent(prop,callback) {
+    count++
+    console.log(`button clicked ${count} times`)
+    callback(prop)
+}
+
+function popup(str){
+    const msg = `you have clicked ${str}`
+    window.alert(msg)
+}
+
--- testing/script.test.js
+++ testing/script.test.js
@@ -5,13 +5,17 @@
 const fs = require('fs');
 const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
-describe('Testing script.js', () => {
-    beforeAll(() => {
-        document.documentElement.innerHTML = html.toString();
-        const js = require('../script')
-    })
 
-    test('Button click changes output content', () => {
+describe('Button1 click ', () => {
+    let js
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+        js = require('../script')
+    })
+    afterEach(() => {
+        document.getElementsByTagName('html')[0].innerHTML = '';
+    });
+    it('changes output content', () => {
         const myButton1 = document.getElementById('button-test1')
         const output = document.getElementById('output')
         const outputBefore = output.textContent
@@ -19,14 +23,25 @@ describe('Testing script.js', () => {
         const outputAfter = output.textContent
         expect(outputBefore).not.toBe(outputAfter)
     });
-
-    test('Button click changes background colour', () => {
+});
+describe('Button2 click ', () => {
+    let js
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+        js = require('../script')
+    })
+    afterEach(() => {
+        document.getElementsByTagName('html')[0].innerHTML = '';
+    });
+    it('changes background colour', () => {
         const myButton2 = document.getElementById('button-test2')
-        const bodyBefore = document.body.style.backgroundColor
-        console.log(bodyBefore)
+        const body = document.querySelector('body')
+        const bodyBefore = body.style.backgroundColor
+        console.log(body)
+        console.log("bodyBefore",bodyBefore)
         myButton2.click()
-        const bodyAfter = document.body.style.backgroundColor
+        const bodyAfter = body.style.backgroundColor
+        console.log("bodyAfter",bodyAfter)
         expect(bodyBefore).not.toBe(bodyAfter)
     });
-
 });
